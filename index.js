d41c6c7b0a225ba38e709bbb87f306048d54c411
FuriousNayan <npatel24@my.waketech.edu>
2025-01-17 13:48:17 -0500

    Adding in Variable Notes 1/17/25

---
+++ index.js
@@ -0,0 +1,38 @@
+// variables
+let x; // Declaration
+console.log(x)
+const y = 10; //Initialization
+console.log(y)
+let z = 3; // Both at the same time
+
+x = "Hello"
+console.log(x)
+
+z = true;
+console.log(z)
+
+let studentName = window.prompt("What is your name?")
+
+console.log(studentName);
+
+window.alert(`Hello, your name is ${studentName}`) //Need backticks in order to text format
+
+let online = false;
+console.log(online)
+console.log(`Online Status ${online}`)
+
+let className = "WEB-115";
+console.log(className);
+console.log(typeof className)
+console.log(className, typeof className)
+myNumber = 5.5; 
+console.log(typeof myNumber)
+
+let myAge = window.prompt("How old are you?")
+console.log(typeof myAge)
+myAge = Number(myAge)
+console.log(myAge, typeof myAge)
+
+let t = "pizza";
+x = Number(t);
+console.log(typeof t)
